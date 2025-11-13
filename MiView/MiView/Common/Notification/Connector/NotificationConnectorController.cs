8c705ff175c2c653b3cc8ffa7fe0408389c9dce9
machi <manche.m.madding.4132.fasternofat@gmail.com>
2025-11-13 20:20:36 +0900

    外部連携コネクタ用のベースクラスを作る

---
+++ MiView/MiView/Common/Notification/Connector/NotificationConnectorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiView.Common.Notification.Connector
+{
+    /// <summary>
+    /// 外部出力連携
+    /// </summary>
+    internal class NotificationConnectorController : NotificationController
+    {
+        public override void ExecuteMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override NotificationControlForm GetControllerForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
