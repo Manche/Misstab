8824aef91240eff999880e15164a2d1d77bb02ce
machi <manche.m.madding.4132.fasternofat@gmail.com>
2025-11-26 22:39:54 +0900

    NotificationConnectorを抽象型にする(派生の派生化)

--- MiView/MiView/Common/Notification/Connector/NotificationConnectorController.cs
+++ MiView/MiView/Common/Notification/Connector/NotificationConnectorController.cs
@@ -9,21 +9,7 @@ namespace MiView.Common.Notification.Connector
     /// <summary>
     /// 外部出力連携
     /// </summary>
-    internal class NotificationConnectorController : NotificationController
+    public abstract class NotificationConnectorController : NotificationController
     {
-        public override void ExecuteMethod()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override NotificationControlForm GetControllerForm()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string ToString()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
