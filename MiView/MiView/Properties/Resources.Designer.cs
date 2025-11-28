1e9f0d8c66b00bb509f0c29d38509f7db652b2c7
machi <manche.m.madding.4132.fasternofat@gmail.com>
2025-11-28 15:18:26 +0900

    アプリケーション名称変更

--- MiView/MiView/Properties/Resources.Designer.cs
+++ MiView/MiView/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MiView.Properties {
+namespace Misstab.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MiView.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MiView.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Misstab.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- MiView/MiView/Properties/Settings.Designer.cs
+++ MiView/MiView/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MiView.Properties {
+namespace Misstab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
