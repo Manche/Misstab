23dde5413e11c1f57f4910299d5593823c364fb4
machi <manche.m.madding.4132.fasternofat@gmail.com>
2026-02-16 11:50:19 +0900

    統計格納先の変更

---
+++ Misstab/Misstab/Common/TimeLine/Stastics/TimeLineStastics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misstab.Common.TimeLine.Stastics
+{
+    public class TimeLineStastics
+    {
+        /// <summary>
+        /// タブ識別値
+        /// </summary>
+        public string? TabDefinition { get; set; }
+        /// <summary>
+        /// タブ名
+        /// </summary>
+        public string? TabName {  get; set; }
+        /// <summary>
+        /// 最終更新日時
+        /// </summary>
+        public DateTime? LatestUpdate { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// ノート数
+        /// </summary>
+        public int NoteCount { get; set; } = 0;
+        /// <summary>
+        /// 内部ノート数
+        /// </summary>
+        public int AllNoteCount { get; set; } = 0;
+        public DateTime LatestPostUpdate {  get; set; } = DateTime.MinValue;
+        public int MemorySize { get; set; } = 0;
+    }
+}
